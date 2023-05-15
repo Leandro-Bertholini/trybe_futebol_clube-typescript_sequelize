402ffd8e120db230ab356a6d673d4d637d84250c
Leandro Bertholini <bertholinileandro1@gmail.com>
2023-05-15 19:18:42 -0300

    alteração: na classe teamService

--- app/backend/src/services/teamService.ts
+++ app/backend/src/services/teamService.ts
@@ -1,14 +1,15 @@
-import { ITeamFull } from '../interfaces/ITeam';
+// import { ITeamFull } from '../interfaces/ITeam';
+import IStatusMessage from '../interfaces/IStatusMessage';
 import TeamModel from '../database/models/TeamModel';
+import { statusResponse } from '../utils/rotesResponses';
 // import IStatusMessage from '../interfaces/IResponseStatus';
-// import IBelongsToService from './servicesInterfaces/teamsMethods';
 
 export default class TeamService {
   constructor(private _teamModel = TeamModel) {}
 
-  public async getAll(): Promise<ITeamFull[]> {
+  public async getAll(): Promise<IStatusMessage> {
     const allTeams = await this._teamModel.findAll();
-    console.log(allTeams);
-    return allTeams;
+    // console.log(allTeams);
+    return statusResponse(200, allTeams);
   }
 }
