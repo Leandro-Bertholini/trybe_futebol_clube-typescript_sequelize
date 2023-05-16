089183652fa8280cd6d4c7b3a67a86eb4640f4e8
Leandro Bertholini <bertholinileandro1@gmail.com>
2023-05-15 21:53:17 -0300

    feat: alteração no file teamRouter e rota concluida de getById

--- app/backend/src/routes/teamRouter.ts
+++ app/backend/src/routes/teamRouter.ts
@@ -6,6 +6,6 @@ const route = Router();
 const teamController = new TeamController();
 
 route.get('/', (req, res) => teamController.getAll(req, res));
-route.get('/:id', (req, res) => teamController.getAll(req, res));
+route.get('/:id', (req, res) => teamController.getById(req, res));
 
 export default route;
