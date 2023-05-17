8062854d19772157faa30924d7d02578175b7d52
Leandro Bertholini <bertholinileandro1@gmail.com>
2023-05-17 14:30:05 -0300

    feat; alteração no loginRouter

--- app/backend/src/routes/loginRouter.ts
+++ app/backend/src/routes/loginRouter.ts
@@ -1,11 +1,17 @@
 import { Router } from 'express';
-import loginController from '../controllers/loginController';
+import LoginController from '../controllers/loginController';
 import loginFieldValidation from '../middlewares/loginFieldValidation';
+import loginCharactersValidation from '../middlewares/loginCharactersValidation';
 
 const route = Router();
 
-const loginController = new loginController();
+const loginController = new LoginController();
 
-route.post('/', loginFieldValidation, loginController);
+route.post(
+  '/',
+  loginFieldValidation,
+  loginCharactersValidation,
+  loginController.authorizationToken,
+);
 
 export default route;
