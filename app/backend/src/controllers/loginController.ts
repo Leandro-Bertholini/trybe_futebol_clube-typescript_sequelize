300347fb13e7b74736d533273b9c4204051106b5
Leandro Bertholini <bertholinileandro1@gmail.com>
2023-05-17 12:16:14 -0300

    feat; criação da camada controller da rota login

--- app/backend/src/controllers/loginController.ts
+++ app/backend/src/controllers/loginController.ts
@@ -1,5 +1,13 @@
-// import { Request, Response } from 'express';
+import { Request, Response } from 'express';
+import LoginService from '../services/loginService';
+import ILogin from '../interfaces/ILogin';
 
-// export default class loginController {
-//   constructor(){};
-// }
+export default class loginController {
+  constructor(private _loginService = new LoginService()) {}
+
+  public async authorizationToken(req: Request, res: Response): Promise<void> {
+    const { status, message } = await this._loginService.authorizationToken(req.body as ILogin);
+
+    res.status(status).json(message);
+  }
+}
--- app/backend/src/routes/loginRouter.ts
+++ app/backend/src/routes/loginRouter.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import loginController from '../controllers/loginController';
+import loginFieldValidation from '../middlewares/loginFieldValidation';
 
 const route = Router();
 
 const loginController = new loginController();
 
-route.post('/', loginController);
+route.post('/', loginFieldValidation, loginController);
 
 export default route;
