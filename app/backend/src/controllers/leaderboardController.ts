271d3c4e6d8d9057a544065e61aa6214df7db7ba
Leandro Bertholini <bertholinileandro1@gmail.com>
2023-05-23 12:45:56 -0300

    feat: criar o controller para getAll da classificação, ainda sem a criação da query tipo "consulta bruta" conforme a documentação do sequelize

---
+++ app/backend/src/controllers/leaderboardController.ts
@@ -0,0 +1,12 @@
+import { Request, Response } from 'express';
+import LeaderboardService from '../services/leaderboardService';
+
+export default class leaderboardController {
+  constructor(private _leaderboard = new LeaderboardService()) {}
+
+  public async getAll(_req: Request, res: Response) {
+    const { status, data } = await this._leaderboard.getAll();
+
+    res.status(status).json(data);
+  }
+}
