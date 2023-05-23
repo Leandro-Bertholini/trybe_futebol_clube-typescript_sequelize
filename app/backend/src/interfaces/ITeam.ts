c426743aca7e650ee02d99d18cfa01d692f5f18a
Leandro Bertholini <bertholinileandro1@gmail.com>
2023-05-23 12:37:16 -0300

    feat: criação da interface para a tabela de classificação

--- app/backend/src/interfaces/ITeam.ts
+++ app/backend/src/interfaces/ITeam.ts
@@ -5,3 +5,16 @@ export default interface ITeamName {
 export interface ITeamFull extends ITeamName{
   id: number,
 }
+
+export interface ITeamData {
+  name: string,
+  totalPoints: number,
+  totalGames: number,
+  totalVictories: number,
+  totalDraws: number,
+  totalLosses: number,
+  goalsFavor: number,
+  goalsOwn: number,
+  goalsBalance?: number,
+  efficiency?: string
+}
