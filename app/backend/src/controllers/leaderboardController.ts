dfad5f8c0dd47475b529ef8dc9e949a611212bb4
Leandro Bertholini <bertholinileandro1@gmail.com>
2023-05-24 19:36:21 -0300

    feat: desenvolvimento da query e controller para leaderboard/home

--- app/backend/src/controllers/leaderboardController.ts
+++ app/backend/src/controllers/leaderboardController.ts
@@ -4,9 +4,17 @@ import LeaderboardService from '../services/leaderboardService';
 export default class LeaderboardController {
   constructor(private _leaderboard = new LeaderboardService()) {}
 
-  public async allHomeTeams(_req: Request, res: Response): Promise<Response> {
-    const { status, data } = await this._leaderboard.allHomeTeams();
+  public async performanceAllHomeTeams(req: Request, res: Response): Promise<Response> {
+    const { path } = req;
+    const teamsData = await this._leaderboard.performanceAllHomeTeams(path);
 
-    return res.status(status).json(data);
+    return res.status(200).json(teamsData);
   }
+
+  // public async performanceAllAwayTeams(req: Request, res: Response): Promise<Response> {
+  //   const params = req.path.split('/')[1];
+  //   const { status, data } = await this._leaderboard.performanceAllAwayTeams(params);
+
+  //   return res.status(status).json(data);
+  // }
 }
